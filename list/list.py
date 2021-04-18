e353ef73495eb7eb40e1be51108c9d30701780ea
isyankarcoder <gencvolkan@gmail.com>
2021-04-17 17:08:50 -0700

    liste

---
+++ list/list.py
@@ -0,0 +1,12 @@
+list4= [10,20,32,0]
+print(len(list4))
+
+list=["Ankara","Istabul","Samsun"]
+
+list5 = [10,20,list,list4]
+print(list5[2:])
+print(list5[:3])
+
+print(list5.pop(3).pop(0))
+
+k="150"
